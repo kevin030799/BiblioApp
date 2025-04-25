b4970411d47482b16a84d7441e3355b52c8b1887
kevin030799 <42698483+kevin030799@users.noreply.github.com>
2025-04-25 15:27:21 -0600

    Login Model

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ser un usuario válido")]
+
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+
+        public string Contraseña { get; set; }
+    }
+}
